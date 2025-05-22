d2e96dac2267b0e5632c20b276166bbb93297f2c
nadim-bestpeeers <namansuri@bestpeers.com>
2025-05-22 14:29:01 +0530

    update navbar : change app name

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,8 +5,8 @@ import { Menu, Search } from 'lucide-react';
 export default function Navbar() {
   return (
     <nav className="absolute top-0 w-full z-50 px-8 py-4 flex justify-between items-center text-white">
-      <h1 className="text-2xl font-bold tracking-wide">Indore Explore</h1>
-      <div className="hidden md:flex gap-6 text-sm font-medium">
+      <h1 className="text-3xl font-bold tracking-wide">Travel Vibe</h1>
+      <div className="hidden md:flex  gap-6  text-sm font-medium">
         <a href="#">Home</a>
         <a href="#">Places</a>
         <a href="#">Culture</a>
